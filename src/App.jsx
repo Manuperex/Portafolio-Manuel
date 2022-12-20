b1b897fbf26a8ff16fc978c571189550581199b3
Manuperex <manuperex24@gmail.com>
2022-12-20 14:42:50 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import SideBar from './Components/SideBar/SideBar'
+import First from './Components/First.jsx/First'
+
+function App() {
+  return (
+    <div className="App">
+      <SideBar />
+      <First />
+    </div>
+  )
+}
+
+export default App
