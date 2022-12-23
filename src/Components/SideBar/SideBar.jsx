edcf4af82a794395e09776e7c1e565c20623e0df
Manuperex <manuperex24@gmail.com>
2022-12-22 20:43:27 -0500

    modifique las url de las imagenes, se cambio de public a assect

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import "./SideBar.css"
 import home from "../../assets/SideBar/home.svg"
+import user from "../../assets/SideBar/user.svg"
+import portfolio from "../../assets/SideBar/portfolio.svg"
+import graduationCap from "../../assets/SideBar/graduation-cap.svg"
+import edificio from "../../assets/SideBar/edificio.svg"
+import chat from "../../assets/SideBar/chat.svg"
 const SideBar = () => {
   return (
     <div className='SideBar'>
@@ -8,19 +13,19 @@ const SideBar = () => {
         <a href="#"><img src={home} alt="#" /></a>
       </div>
       <div>
-        <a href="#"><img src="../../assets/SideBar/user.svg" alt="" /></a>
+        <a href="#"><img src={user} alt="" /></a>
       </div>
       <div>
-        <a href="#"><img src=".../../assets/SideBar/portfolio.svg" alt="" /></a>
+        <a href="#"><img src={portfolio} alt="" /></a>
       </div>
       <div>
-        <a href="#"><img src="../../../public/SideBar/graduation-cap.svg" alt="" /></a>
+        <a href="#"><img src={graduationCap} alt="" /></a>
       </div>
       <div>
-        <a href="#"><img src="../../../public/SideBar/edificio.svg" alt="" /></a>
+        <a href="#"><img src={edificio} alt="" /></a>
       </div>
       <div>
-        <a href="#"><img src="../../../public/SideBar/chat.svg" alt="" /></a>
+        <a href="#"><img src={chat} alt="" /></a>
       </div>
 
     </div>
